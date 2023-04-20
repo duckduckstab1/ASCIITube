1fcf6eb123381dd52bb55081ec9746eca69b8360
hurricane495 <84234115+duckduckstab1@users.noreply.github.com>
2023-04-20 03:26:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,125 @@
-import cv2
+import math
 
-from PIL import Image, ImageOps
+import struct
 
-video = cv2.VideoCapture('video.mp4')
+import sys
 
-while True:
+import time
 
-    ret, frame = video.read()
+from PIL import Image
 
-    if not ret:
+# Define the ASCII characters to use for the video conversion
 
-        break
+ascii_chars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# Function to convert a single image to ASCII
 
-    size = (80, 60)
+def image_to_ascii(image, width, height):
 
-    small_gray = cv2.resize(gray, size)
+    # Resize the image and convert it to grayscale
 
-    inverted = cv2.bitwise_not(small_gray)
+    image = image.resize((width, height)).convert('L')
 
-    image = Image.fromarray(inverted)
+    # Convert each pixel to an ASCII character based on its brightness
 
-    ascii_image = ImageOps.invert(image.convert('L')).convert('1')
+    ascii_pixels = []
 
-    print(ascii_image)
+    for y in range(height):
 
-video.release()
+        ascii_row = []
 
+        for x in range(width):
+
+            brightness = image.getpixel((x,y))
+
+            ascii_index = math.ceil((brightness / 255) * (len(ascii_chars) - 1))
+
+            ascii_row.append(ascii_chars[ascii_index])
+
+        ascii_pixels.append(''.join(ascii_row))
+
+    # Combine the ASCII characters into a single string
+
+    ascii_art = '\n'.join(ascii_pixels)
+
+    return ascii_art
+
+# Function to convert a video file to ASCII frames
+
+def video_to_ascii(video_file):
+
+    # Open the video file and get its properties
+
+    with open(video_file, 'rb') as f:
+
+        f.seek(0, 2)
+
+        file_size = f.tell()
+
+        f.seek(0)
+
+        width = struct.unpack('i', f.read(4))[0]
+
+        height = struct.unpack('i', f.read(4))[0]
+
+        frame_size = width * height * 3
+
+        # Define the number of frames to process at a time
+
+        frames_per_chunk = 100
+
+        while True:
+
+            # Read a chunk of frames from the video
+
+            chunk_start = f.tell()
+
+            chunk_data = f.read(frame_size * frames_per_chunk)
+
+            if not chunk_data:
+
+                break
+
+            chunk_end = f.tell()
+
+            # Convert each frame in the chunk to ASCII art
+
+            for i in range(frames_per_chunk):
+
+                # Calculate the offset of the current frame in the chunk
+
+                frame_offset = i * frame_size
+
+                # Extract the frame data from the chunk
+
+                frame_data = chunk_data[frame_offset:frame_offset+frame_size]
+
+                # Convert the frame data to a PIL Image object
+
+                frame = Image.frombytes('RGB', (width, height), frame_data)
+
+                # Convert the frame to ASCII art
+
+                ascii_art = image_to_ascii(frame, 80, 60)
+
+                # Print the ASCII art to the console
+
+                sys.stdout.write('\033[2J\033[1;1H' + ascii_art)
+
+                sys.stdout.flush()
+
+            # Move the file pointer to the start of the next chunk
+
+            f.seek(chunk_end)
+
+            # Wait for the appropriate amount of time before displaying the next frame
+
+            time.sleep(1/30)  # Assumes a frame rate of 30fps
+
+    # Close the video file
+
+    f.close()
+
+# Convert the video file to ASCII art
+
+video_to_ascii('video.mp4')
